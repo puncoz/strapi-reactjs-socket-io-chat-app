efa2ff404c63c7becc07966413f665a18375eb9c
Puncoz Nepal <puncoz138@gmail.com>
2021-03-10 22:13:49 +0545

    strapi scaffold

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a97c833b6dea6d362448f6d467f55348'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '484339f0-eba8-4d41-af18-af6a1af976b6'
+};
